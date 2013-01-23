2337ede3e0aec6118301e0ecf0f2cc89707f84be
BigHistory.me <BigHistory.me@gmail.com>
2013-01-23 14:45:08 -0800

    finished time reversal code, including sprite movement & mirror X coord on reverse (timejumps don't preserve state)

--- common.js
+++ common.js
@@ -409,6 +409,47 @@ var Game = {  // a modified version of the game loop from my previous boulderdas
             Dom.storage.muted = music.muted = !music.muted;
             Dom.toggleClassName('mute', 'on', music.muted);
         });
+    },
+
+    exit: function( status ) {
+        // http://kevin.vanzonneveld.net
+        // +   original by: Brett Zamir (http://brettz9.blogspot.com)
+        // +      input by: Paul
+        // +   bugfixed by: Hyam Singer (http://www.impact-computing.com/)
+        // +   improved by: Philip Peterson
+        // +   bugfixed by: Brett Zamir (http://brettz9.blogspot.com)
+        // %        note 1: Should be considered expirimental. Please comment on this function.
+        // *     example 1: exit();
+        // *     returns 1: null
+
+        var i;
+
+        if (typeof status === 'string') {
+            alert(status);
+        }
+
+        window.addEventListener('error', function (e) {e.preventDefault();e.stopPropagation();}, false);
+
+        var handlers = [
+            'copy', 'cut', 'paste',
+            'beforeunload', 'blur', 'change', 'click', 'contextmenu', 'dblclick', 'focus', 'keydown', 'keypress', 'keyup', 'mousedown', 'mousemove', 'mouseout', 'mouseover', 'mouseup', 'resize', 'scroll',
+            'DOMNodeInserted', 'DOMNodeRemoved', 'DOMNodeRemovedFromDocument', 'DOMNodeInsertedIntoDocument', 'DOMAttrModified', 'DOMCharacterDataModified', 'DOMElementNameChanged', 'DOMAttributeNameChanged', 'DOMActivate', 'DOMFocusIn', 'DOMFocusOut', 'online', 'offline', 'textInput',
+            'abort', 'close', 'dragdrop', 'load', 'paint', 'reset', 'select', 'submit', 'unload'
+        ];
+
+        function stopPropagation (e) {
+            e.stopPropagation();
+            // e.preventDefault(); // Stop for the form controls, etc., too?
+        }
+        for (i=0; i < handlers.length; i++) {
+            window.addEventListener(handlers[i], function (e) {stopPropagation(e);}, true);
+        }
+
+        if (window.stop) {
+            window.stop();
+        }
+
+        throw '';
     }
 
 };
@@ -529,12 +570,16 @@ var Render = {
         // don't really want to have to recalc r1 (already did it for segment), maybe figure out a way to save
         var fontsize = Math.round(scale * 300000);
         if (fontsize > (y1 - clipY)) {
-            var r1 = Math.round(Render.rumbleWidth(w1, lanes));
+            //var r1 = Math.round(Render.rumbleWidth(w1, lanes));
             //console.log(fontsize, x1+w1+r1, y1, clipY);
             ctx.save();
             ctx.fillStyle = color;
             ctx.font = fontsize + "px sans-serif";
-            ctx.fillText(timeMarker, x1 + w1 + r1, y1);
+            if (timeBackward)
+                ctx.fillText(timeMarker, x1 + w1, y1);
+            else
+                ctx.fillText(timeMarker, x1 - w1, y1);
+            //ctx.fillText(timeMarker, x1 + w1 + r1, y1);
             ctx.restore();
         }
 
